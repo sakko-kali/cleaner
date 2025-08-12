abe1f78139770283186127d65bf615f323c8b59e
Sako-kali <sarkisssbarsegian@gmail.com>
2025-08-12 21:51:11 +0400

    v2

--- cleaner.sh
+++ cleaner.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
-cd ~/Загрузки
 
-mkdir -p "Фото" "Архивы" "Видео" "Файлы" "Прочее"
+case $1 in
+  "eng"|"Eng")
+    cd ~/Downloads
+    mkdir -p "Photo" "Archives" "Video" "Documents" "Others" "Scripts";;
+  *)
+    cd ~/Загрузки
+    mkdir -p "Фото" "Архивы" "Видео" "Прочее" "Документы" "Скрипты";;
+esac
+
+
 
 while true; do
    array=(*)
    for i in "${array[@]}"; do
      if [[ -f "$i" && ! "$i" == *.crdownload ]]; then
-       case "$i" in
-        *.png|*.jpg|*.jpeg|*.gif)
-            mv "$i" Фото;;
-        *.zip|*.7zip|*.rar|*.gz|*.tar)
-            mv "$i" Архивы;;
-        *.mp4|*.webm|*.mov|*.mkv)
-            mv "$i" Видео;;
-         *.txt|*.pdf|*.docx)
-            mv "$i" Файлы;;
-         *)
-            mv "$i" Прочее;;
-         esac
+       if [[ $1 ]]; then
+         case "$i" in
+          *.png|*.jpg|*.jpeg|*.gif)
+              mv -n "$i" Photo;;
+          *.zip|*.7zip|*.rar|*.gz|*.tar)
+              mv -n "$i" Archives;;
+          *.mp4|*.webm|*.mov|*.mkv)
+              mv -n "$i" Video;;
+           *.txt|*.pdf|*.docx)
+              mv -n "$i" Documents;;
+           *.sh|*.py|*.js|*.php|*.rb|*.pl|*.lua|*.md)
+              mv -n "$i" Scripts;;
+           *)
+              mv -n "$i" Others;;
+           esac
+       else
+         case "$i" in
+          *.png|*.jpg|*.jpeg|*.gif)
+              mv -n "$i" Фото;;
+          *.zip|*.7zip|*.rar|*.gz|*.tar)
+              mv -n "$i" Архивы;;
+          *.mp4|*.webm|*.mov|*.mkv)
+              mv -n "$i" Видео;;
+           *.txt|*.pdf|*.docx)
+              mv -n "$i" Документы;;
+           *.sh|*.py|*.js|*.php|*.rb|*.pl|*.lua|*.md)
+              mv -n "$i" Скрипты;;
+           *)
+              mv -n "$i" Прочее;;
+           esac
+      fi
      fi
    done
    sleep 15
