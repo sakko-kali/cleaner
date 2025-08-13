fd92714d6ff2aae2fc892d5d84e3efc3ef6b8331
Sako-kali <sarkisssbarsegian@gmail.com>
2025-08-13 23:15:55 +0400

    v2.2 update

--- cleaner.sh
+++ cleaner.sh
@@ -11,40 +11,96 @@ case $1 in
 esac
 
 
-
+##############################################
 while true; do
-   array=(*)
+   array=(*)                                                    #getting array of current files
    for i in "${array[@]}"; do
-     if [[ -f "$i" && ! "$i" == *.crdownload ]]; then
-       if [[ $1 ]]; then
+     if [[ -f "$i" && ! "$i" == *.crdownload ]]; then           #skip files for download in real time
+       if [[ $1 ]]; then                                        #Checking for english directory
          case "$i" in
           *.png|*.jpg|*.jpeg|*.gif)
-              mv -n "$i" Photo;;
+              if [[ -f "Photo/$i" ]]; then                       #if file has same name with current file - change it
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Photo/"$name"
+              else
+                mv -n "$i" Photo
+              fi;;
           *.zip|*.7zip|*.rar|*.gz|*.tar)
-              mv -n "$i" Archives;;
+              if [[ -f "Archives/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Archives/"$name"
+              else
+                mv -n "$i" Archives
+              fi;;
           *.mp4|*.webm|*.mov|*.mkv)
-              mv -n "$i" Video;;
+              if [[ -f "Video/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Video/"$name"
+              else
+                mv -n "$i" Video
+              fi;;
            *.txt|*.pdf|*.docx)
-              mv -n "$i" Documents;;
+              if [[ -f "Documents/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Documents/"$name"
+              else
+                mv -n "$i" Documents
+              fi;;
            *.sh|*.py|*.js|*.php|*.rb|*.pl|*.lua|*.md)
-              mv -n "$i" Scripts;;
+              if [[ -f "Scripts/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Scripts/"$name"
+              else
+                mv -n "$i" Scripts
+              fi;;
            *)
-              mv -n "$i" Others;;
+              if [[ -f "Others/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Others/"$name"
+              else
+                mv -n "$i" Others
+              fi;;
            esac
        else
          case "$i" in
           *.png|*.jpg|*.jpeg|*.gif)
-              mv -n "$i" Фото;;
+              name="${i%.*}_$(date +%T).${i##*.}"
+              mv -n "$i" Фото/"$name";;
           *.zip|*.7zip|*.rar|*.gz|*.tar)
-              mv -n "$i" Архивы;;
+              if [[ -f "Архивы/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Архивы/"$name"
+              else
+                mv -n "$i" Архивы
+              fi;;
           *.mp4|*.webm|*.mov|*.mkv)
-              mv -n "$i" Видео;;
+              if [[ -f "Видео/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Видео/"$name"
+              else
+                mv -n "$i" Видео
+              fi;;
            *.txt|*.pdf|*.docx)
-              mv -n "$i" Документы;;
+              if [[ -f "Документы/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Документы/"$name"
+              else
+                mv -n "$i" Документы
+              fi;;
            *.sh|*.py|*.js|*.php|*.rb|*.pl|*.lua|*.md)
-              mv -n "$i" Скрипты;;
+              if [[ -f "Скрипты/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Скрипты/"$name"
+              else
+                mv -n "$i" Скрипты
+              fi;;
            *)
-              mv -n "$i" Прочее;;
+              if [[ -f "Прочее/$i" ]]; then
+                name="${i%.*}_$(date +%T).${i##*.}"
+                mv -n "$i" Прочее/"$name"
+              else
+                mv -n "$i" Прочее
+              fi;;
            esac
       fi
      fi
